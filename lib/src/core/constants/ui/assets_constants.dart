c15f5cae65858788ce6c92d457e3dee25d23f2d5
princeteck <privateyuvraj@gmail.com>
2025-07-26 13:05:25 +0530

    base icons added for ui design of dashboard

--- lib/src/core/constants/ui/assets_constants.dart
+++ lib/src/core/constants/ui/assets_constants.dart
@@ -5,5 +5,24 @@ class KImages {
 }
 
 class KIcons {
-  static const String logoIcon = 'assets/icons/logo.svg';
+  static const String arrowLeft = 'assets/icons/arrow-left.svg';
+  static const String arrowRight = 'assets/icons/arrow-right.svg';
+  static const String category = 'assets/icons/category.svg';
+  static const String chart = 'assets/icons/chart.svg';
+  static const String chartCircular = 'assets/icons/chart-circular.svg';
+  static const String documentText = 'assets/icons/document-text.svg';
+  static const String edit = 'assets/icons/edit.svg';
+  static const String export = 'assets/icons/export.svg';
+  static const String exportUp = 'assets/icons/export-up.svg';
+  static const String grid = 'assets/icons/grid.svg';
+  static const String heart = 'assets/icons/heart.svg';
+  static const String home = 'assets/icons/home.svg';
+  static const String logo = 'assets/icons/logo.svg';
+  static const String routing = 'assets/icons/routing.svg';
+  static const String search = 'assets/icons/search.svg';
+  static const String setting = 'assets/icons/setting.svg';
+  static const String spark = 'assets/icons/spark.svg';
+  static const String star = 'assets/icons/star.svg';
+  static const String trophy = 'assets/icons/trophy.svg';
+  static const String user = 'assets/icons/user.svg';
 }
